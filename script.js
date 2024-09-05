0a7b60f6c84b5afb9658891b7b969f4afcf7f18d
Terrana Willma <terrana.w@gmail.com>
2024-09-05 14:25:59 +0700

    edit

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function btnCalculate() {
     
     let bmi = weight / ((height/100)*(height/100))
 
-    if (bmi < 18.5) {
+    if (bmi < 18.5 && bmi > 0) {
         document.getElementById('result').innerHTML = "Your BMI is <strong>" + bmi.toFixed(1) + "</strong> which means you are <strong>Underweight</strong>"
     }
     else if (bmi >= 18.5 && bmi <=24.9) {
@@ -13,8 +13,10 @@ function btnCalculate() {
     else if (bmi >24.9 && bmi <=29.9)   {
         document.getElementById('result').innerHTML = "Your BMI is <strong>" + bmi.toFixed(1) + "</strong> which means you are <strong>Overweight</strong>"
     }
-    else {
+    else if (bmi >= 30){
         document.getElementById('result').innerHTML = "Your BMI is <strong>" + bmi.toFixed(1) + "</strong> which means you are <strong>Obesity</strong>"
-
     }
+    else{
+        document.getElementById('result').innerHTML = "Please input your weight & height"
+}
 }
